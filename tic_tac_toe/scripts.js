51730a5093779f547aaedfd38532fc984df30d72
pokiujf <arkadiusz.machalica@gmail.com>
2016-01-20 19:47:15 +0000

    disabled buttons

--- tic_tac_toe/scripts.js
+++ tic_tac_toe/scripts.js
@@ -1,7 +1,16 @@
 $(document).ready(function() {
+    
+    var current_sign = "X";
+    $('h1').text('Teraz kolejka: ' + current_sign);
 
-    $('a').on('click', function() {
-       alert('click!')
+    $('button').on('click', function() {
+        if(current_sign == 'X') {
+            $(this).text(current_sign).attr("disabled", true);
+            current_sign = 'O';
+        } else {
+            $(this).text(current_sign).attr("disabled", true);
+            current_sign = 'X';
+        }
     });
 
 });
