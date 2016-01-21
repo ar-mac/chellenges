3ef156e32036cf29ba6c14b110bbb43434acf243
pokiujf <arkadiusz.machalica@gmail.com>
2016-01-21 19:55:47 +0000

    change buttons to table cells

--- tic_tac_toe/scripts.js
+++ tic_tac_toe/scripts.js
@@ -3,14 +3,17 @@ $(document).ready(function() {
     var current_sign = "X";
     $('h1').text('Teraz kolejka: ' + current_sign);
 
-    $('button').on('click', function() {
+    $('td').on('click', function() {
         if(current_sign == 'X') {
-            $(this).text(current_sign).attr("disabled", true);
             current_sign = 'O';
         } else {
-            $(this).text(current_sign).attr("disabled", true);
             current_sign = 'X';
         }
+        $('h1').text('Teraz kolejka: ' + current_sign);
+        $(this).text(current_sign).attr("disabled", true);
     });
-
+    
+    $('#reset_button').on('click', function() {
+        $('td').text('');
+    });
 });
