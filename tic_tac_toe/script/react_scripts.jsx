e964f816eecc17605746aacbddc35f441f937812
A Machalica <a.machalica@selleo.com>
2016-01-24 20:12:44 +0100

    working implementation on React

--- tic_tac_toe/script/react_scripts.jsx
+++ tic_tac_toe/script/react_scripts.jsx
@@ -1,60 +1,105 @@
 var Game = React.createClass({
   getInitialState: function() {
     return{
-      grid: new Array(3*3).fill(''),
-      currentSign: 'X'
+      fields: new Array(3 * 3).fill(''),
+      currentSign: 'X',
+      lines: [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6],
+        [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]],
+      gameInProgress: true,
+      draw: false
     };
   },
-  setupGrid: function() {
-    var data = this.state.grid;
-    var container = [];
-    while(data.length > 0)
-      container.push(data.splice(0, size));
-    return container
+  componentDidMount: function () {
+    $('.cell').click(this.setSign);
+    $('#reset-button').click(this.resetGame);
   },
-  currentRound: function() {
-    return 'Current round: ' + this.state.currentSign
+  setSign: function (event) {
+    var cellActive = !event.target.attributes['disabled'];
+    if (cellActive && this.state.gameInProgress) {
+      var fields = this.state.fields;
+      var index = event.target.dataset['index'];
+      fields[index] = this.state.currentSign;
+      this.setState({fields: fields});
+      this.checkLines(index);
+      if (this.state.gameInProgress) {
+        this.flipSign()
+      }
+    }
+  },
+  resetGame: function () {
+    this.replaceState(this.getInitialState());
+  },
+  checkLines: function (index) {
+    var _this = this;
+    index = parseInt(index);
+    this.state.lines.forEach(function (line) {
+      if (line.indexOf(index) != -1) {
+        var winningLine = line.every(_this.isWinning);
+        if (winningLine) _this.gameWon();
+      }
+    });
+    var allOccupied = this.state.fields.every(function(sign) {
+      return !!sign
+    });
+    debugger;
+    if (allOccupied) {
+      _this.draw();
+    }
+  },
+  draw: function() {
+    this.setState({draw: true})
+  },
+  gameWon: function () {
+    var fields = this.state.fields.map(function (cell) {
+      if (cell === '') {
+        return cell = ' '
+      } else {
+        return cell
+      }
+    });
+    this.setState({fields: fields, gameInProgress: false});
+
+  },
+  isWinning: function (lineIndex) {
+    return this.state.fields[lineIndex] === this.state.currentSign
+  },
+  flipSign: function () {
+    var currentSign = this.state.currentSign === 'X' ? 'O' : 'X';
+    this.setState({currentSign: currentSign})
   },
   render: function() {
-    var gridMarkup = this.setupGrid().map(function(row, index) {
-      var rowId = 'row-' + index;
+    var fieldsMarkup = this.state.fields.map(function (sign, index) {
       return (
-        <Row rowId={rowId} row={row}/>
+        <div key={index} className="cell" data-index={index} disabled={!!sign}>
+          {sign}
+        </div>
       )
     });
 
     return (
-      <div id="react-game">
+      <div>
         <h1>Tic-Tac-Toe</h1>
-        <p>{this.state.currentRound}</p>
-        <table>
-          {gridMarkup}
-        </table>
+        <h2 className="winning-note" hidden={this.state.gameInProgress}>
+          Won by: {this.state.currentSign}
+        </h2>
+        <h2 className="draw-note" hidden={!this.state.draw}>
+          Draw
+        </h2>
+        <button id="reset-button">Reset game</button>
+        <p>
+          <span>
+            Current round: {this.state.currentSign}
+          </span>
+        </p>
+        <div className="game">
+          {fieldsMarkup}
+        </div>
       </div>
     );
   }
 });
 
-var Row = React.createClass({
-  propTypes: {
-    rowId: React.PropTypes.integer.required,
-    row: React.PropTypes.array.required
-  },
-  render: function() {
-
-
-
-    return (
-      <tr id={this.prop.rowId}>
-
-      </tr>
-    )
-  }
-});
-
-
-
 ReactDOM.render(
   <Game />,
-  document.getElementById('con')
+  document.getElementById('content')
 );
