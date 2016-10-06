9549c96effb5810c21479a7437671e44c5cee5a0
A Machalica <a.machalica@selleo.com>
2016-10-06 19:21:35 +0200

    Adds Dragger and main.js

---
+++ dragging_box/scripts/Dragger.jsx
@@ -0,0 +1,23 @@
+import React, {PropTypes} from 'react';
+
+export default class Dragger extends React.Component {
+  static propTypes = {
+
+  };
+
+  constructor(props, context) {
+    super(props, context);
+
+    this.state = {
+
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        Here be dragger
+      </div>
+    )
+  }
+}
---
+++ dragging_box/scripts/main.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import Dragger from './Dragger';
+
+React.render(<Dragger />, document.getElementsByTagName('body')[0]);
+
