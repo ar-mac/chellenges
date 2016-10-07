3b1385f6457da8ad08977ff5f3d497f75ecbef5c
A Machalica <a.machalica@selleo.com>
2016-10-07 08:32:34 +0200

    Functional dragger

--- dragging_box/scripts/Dragger.jsx
+++ dragging_box/scripts/Dragger.jsx
@@ -5,16 +5,34 @@ export default class Dragger extends React.Component {
     super(props, context);
 
     this.state = {
-      onHold: false
+      onHold: false,
+      begin: false,
+      offsetX: 0,
+      offsetY: 0,
+      leftMargin: 0,
+      topMargin: 0
     };
   }
 
-  setOnHold = () => {
-    this.setState({onHold: true});
+  setOnHold = (e) => {
+    this.setState({onHold: true, begin: true});
+    document.addEventListener("mousemove", this.drag)
   };
 
   clearOnHold = () => {
     this.setState({onHold: false});
+    document.removeEventListener("mousemove", this.drag)
+  };
+
+  drag = (e) => {
+    if (this.state.begin) {
+      this.setState({offsetX: e.offsetX, offsetY: e.offsetY, begin: false})
+    } else {
+      this.setState({
+        leftMargin: (e.pageX - this.state.offsetX),
+        topMargin: (e.pageY - this.state.offsetY)
+      })
+    }
   };
 
   classNames = () => {
@@ -26,9 +44,11 @@ export default class Dragger extends React.Component {
   render() {
     return (
       <div
+        ref="dragger"
         className={this.classNames()}
         onMouseDown={this.setOnHold}
         onMouseUp={this.clearOnHold}
+        style={{left: `${this.state.leftMargin}px`, top: `${this.state.topMargin}px`}}
       >
         Here be dragger
       </div>
